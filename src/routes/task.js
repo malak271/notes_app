a77af90cd72c8f5935347e210747faf80da3d411
shahadabulebdah <shahadlebdah@gmail.com>
2023-05-02 12:59:05 +0300

    updated

--- src/routes/task.js
+++ src/routes/task.js
@@ -22,13 +22,13 @@ router.delete('/delete/:id',TaskController.deleteByID)
 
 router.get('/completionPerDay/',TaskController.calculateCompletionPercentagePerDay)
 
+
 router.post('/subTask/:id',verify,subTaskController.insertNewSubTask) //add subtask 
 router.put('/updatesubTask/:taskid/:subtaskid',verify,subTaskController.updateSubTaskByID) //update subtask
 router.delete('/deletesubTask/:taskid/:subtaskid',verify,subTaskController.deleteSubTaskByID) //delete subtask
 router.put('/completedTask/:taskid',verify,subTaskController.checksubTasktocomletedtask)//complete task by check completed subtasl
 router.put('/completedSubTask/:taskid/:subtaskid',verify,subTaskController.subTaskCompleted) //complete subtask
 router.put('/completedAllTasks/:taskid',verify,subTaskController.comletedtask) //complete task and all its subtasks
-
 
 
 module.exports=router
