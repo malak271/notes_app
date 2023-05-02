6a88767844138ab63462051fca25e5919f010574
malak271 <malakghabayen396@gmail.com>
2023-05-02 20:20:14 +0300

    add helper to subTaskCompleted

--- src/controllers/subtaskController.js
+++ src/controllers/subtaskController.js
@@ -1,16 +1,16 @@
-const router=require('express').Router()
-const Task=require("../models/Task")
-const mongoose=require("mongoose");
+const router = require('express').Router()
+const Task = require("../models/Task")
+const mongoose = require("mongoose");
 
 const calculation = require("../helpers/calculation")
 
 
 
-module.exports.insertNewSubTask = async(req,res)=>{
-const {id} = req.params
+module.exports.insertNewSubTask = async (req, res) => {
+  const { id } = req.params
 
-const task = await Task.findById(id)
-task.subtasks.push(req.body)
+  const task = await Task.findById(id)
+  task.subtasks.push(req.body)
 
 
   task.save()
@@ -21,168 +21,178 @@ task.subtasks.push(req.body)
 }
 
 //done
-module.exports.updateSubTaskByID = async(req,res)=>{
+module.exports.updateSubTaskByID = async (req, res) => {
 
-const taskid = req.params.taskid
-const subtaskid = req.params.subtaskid
+  const taskid = req.params.taskid
+  const subtaskid = req.params.subtaskid
 
 
-const task= await Task.findById(taskid)
+  const task = await Task.findById(taskid)
 
-if(task && task.subtasks){
+  if (task && task.subtasks) {
 
-  const subtask = await task.subtasks.filter(subtask => subtask.subtaskId == subtaskid) //return array of one object
-//indexed array
+    const subtask = await task.subtasks.filter(subtask => subtask._id == subtaskid) //return array of one object
+    //indexed array
 
-console.log(subtask)
+    console.log(subtask)
 
-if(req.body.title)
-  subtask[0].title = req.body.title
+    if (req.body.title)
+      subtask[0].title = req.body.title
 
- if(req.body.description)
-  subtask[0].description = req.body.description
+    if (req.body.description)
+      subtask[0].description = req.body.description
 
-  if(req.body.subtaskId)
-   subtask[0].subtaskId = req.body.subtaskId
-  
-  if(req.body.completed)
+    // if (req.body.subtaskId)
+    //   subtask[0].subtaskId = req.body.subtaskId
 
-  subtask[0].completed = req.body.completed
+    if (req.body.completed)
 
-  console.log(task)
-  task.save()
- 
+      subtask[0].completed = req.body.completed
 
-  res.status(200).json("found")
+    console.log(task)
+    task.save()
 
-}
+
+    res.status(200).json("found")
+
+  }
 
 
 }
 
 //done
-module.exports.deleteSubTaskByID = async(req,res)=>{
+module.exports.deleteSubTaskByID = async (req, res) => {
 
-    const taskid = req.params.taskid
-const subtaskid = req.params.subtaskid
+  const taskid = req.params.taskid
+  const subtaskid = req.params.subtaskid
 
-console.log(subtaskid)
+  console.log(subtaskid)
 
-const task= await Task.findById(taskid)
+  const task = await Task.findById(taskid)
 
-if(task && task.subtasks){
+  if (task && task.subtasks) {
 
-const subtask = await task.subtasks.filter(subtask => subtask.subtaskId == subtaskid)
+    const subtask = await task.subtasks.filter(subtask => subtask._id == subtaskid)
 
-console.log(subtask)
+    console.log(subtask)
 
-await Task.findByIdAndUpdate(taskid,{$pull : { subtasks : subtask[0] }} , {new:true})
- 
-
-const utatask = Task.findById(taskid)
-res.status(200).json("done")
+    await Task.findByIdAndUpdate(taskid, { $pull: { subtasks: subtask[0] } }, { new: true })
 
 
-}
+    const utatask = Task.findById(taskid)
+    res.status(200).json("done")
+
+
+  }
 
 }
 
 //completion percentage added 
-module.exports.checksubTasktocomletedtask = async(req,res)=>{
+module.exports.checksubTasktocomletedtask = async (req, res) => { //call this when check task, convert all sub tasks to completed
 
-const {taskid} = req.params
+  const { taskid } = req.params
 
-const task = await Task.findById(taskid)
+  const task = await Task.findById(taskid)
 
-const completedsubtasks= await task.subtasks.every(subtask=>subtask.completed === 1)
-
- console.log(completedsubtasks)
-
- if(completedsubtasks){
+  const completedsubtasks = await task.subtasks.every(subtask => subtask.completed === 1)
 
   console.log(completedsubtasks)
 
- const filter = { _id: taskid };
+  if (completedsubtasks) {
 
- const update = { $set: { completed: 'true'} };
- 
- const result = await Task.updateOne(filter, update)
+    console.log(completedsubtasks)
 
- const completedTask = await Task.findById(taskid)
- 
- res.status(200).json(completedTask)
+    const filter = { _id: taskid };
 
- const calcresult = calculation.overallCompletionPercentage(task)
+    const update = { $set: { completed: 'true' } };
 
-task.completionPercentage = calcresult
+    const result = await Task.updateOne(filter, update)
 
- }
+    const completedTask = await Task.findById(taskid)
 
- res.status(500).json("not all sub tasks are completed")
+    res.status(200).json(completedTask)
+    
+    const calcresult = calculation.overallCompletionPercentage(task)
+
+    task.completionPercentage = calcresult
+
+  }
+
+  res.status(500).json("not all sub tasks are completed")
 
 
 }
 
 //completion percentage added 
-module.exports.comletedtask = async(req,res)=>{
+module.exports.comletedtask = async (req, res) => {
 
-const {taskid} = req.params
+  const { taskid } = req.params
 
-const task = await Task.findById(taskid)
+  const task = await Task.findById(taskid)
 
+  //update every subtask of subtasks
+  const subtaskUpdates = task.subtasks.map(subtask => ({
+    updateMany: {
+      filter: { 'subtasks._id': subtask._id },
+      update: { $set: { 'subtasks.$.completed': 1,'subtasks.$.completionDate': Date.now() } }
+    }
+  }));
 
-//update every subtask of subtasks
- const subtaskUpdates = task.subtasks.map(subtask => ({
-  updateMany: {
-    filter: { 'subtasks._id': subtask._id },
-    update: { $set: { 'subtasks.$.completed': 1 } }
-  }
-}));
+  //update on DB
 
-//update on DB
-
-Task.bulkWrite(subtaskUpdates)
-  .then(result => {
-    console.log('Subtasks updated:', result);
-  })
-  .catch(err => {
-    console.error('Error updating subtasks:', err);
-  });
+  Task.bulkWrite(subtaskUpdates)
+    .then(result => {
+      console.log('Subtasks updated:', result);
+    })
+    .catch(err => {
+      console.error('Error updating subtasks:', err);
+    });
 
   // update completion percentage after update all subtasks
-  const result = calculation.overallCompletionPercentage(task)
+  // const result = calculation.calculateTaskCompletionPercentage(task)
 
-task.completionPercentage = result
+  // console.log(result)
+  task.completionPercentage = 100
+  task.save()
+  res.status(200).json(task)
 
-
- 
 }
 
 //done
 //completion date added 
-module.exports.subTaskCompleted = async(req,res)=>{
-   try{
+module.exports.subTaskCompleted = async (req, res) => {
+  try {
     const taskid = req.params.taskid
     const subtaskid = req.params.subtaskid
-    const subTask = req.body 
+    const subTask = req.body
 
 
     const task = await Task.findById(taskid)
-    const subtask= task.subtasks.filter(subtask=>subtask.subtaskId == subtaskid )
+    const subtask = task.subtasks.filter(subtask => subtask._id == subtaskid)
 
 
-  console.log(subtask)
-  
-      subtask[0].completed = true
-      subtask[0].completionDate = Date.now()
+    console.log(subtask)
 
-      task.save()
+    subtask[0].completed = true
+    subtask[0].completionDate = Date.now()
 
-  res.status(200).json(subtask)
-}catch(error){
-   console.log(error.message)
-   res.status(500).json({message:error.message})
-}
+    //when check subtask the task completion percentage will change
+    const calcresult = calculation.calculateTaskCompletionPercentage(task)
+
+    task.completionPercentage = calcresult
+
+    //if cp=100 front end check it to color the line and check the box
+    
+    if(calcresult==100)
+       task.completionDate=Date.now()
+
+    task.save()
+
+    res.status(200).json(subtask)
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({ message: error.message })
+  }
 
 
 }
