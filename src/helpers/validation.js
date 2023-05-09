8c65e84b8dbe05eb3c6160448ac3b4d5d480b6dd
Ayah Elshawa <ayahs19302@gmail.com>
2023-05-09 15:44:17 +0300

    added regex for password validation

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -7,7 +7,11 @@ const schema=joi.object({
     name: joi.string().min(6).required(),
     email:joi.string().min(6).required().email(),
     //password length between 6 to 20 characters, at least one numeric digit, one uppercase and one lowercase letter
-    password: joi.string().pattern(new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}$/)).message('Password must be >= 7 digits, contains at least one numeric digit, one uppercase and one lowercase letter').required(),
+    password: joi
+            .string()
+            .pattern(new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}$/))
+            .message('Password must be >= 7 digits, contains at least one numeric digit, one uppercase and one lowercase letter')
+            .required(),
 });
 return schema.validate(data)
 }
